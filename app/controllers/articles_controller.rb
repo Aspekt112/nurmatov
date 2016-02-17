303ed5798328871c86402599d6cd9a25be058c58
aspekt112 <aspekt112@gmail.com>
2016-02-17 22:46:01 +0300

    Make a basic User model (including secure passwords)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,12 @@ class ArticlesController < ApplicationController
 	#http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
 	def index
-    	@articles = Article.all
-  	end
-  	
+		@articles = Article.all
+	end
+	
 	def show
-    	@article = Article.find(params[:id])
-  	end
+		@article = Article.find(params[:id])
+	end
 
 	def new
 		@article = Article.new
--- test/controllers/contact_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ContactControllerTest < ActionController::TestCase
-  test "should get contacts" do
-    get :contacts
-    assert_response :success
-  end
-
-end
