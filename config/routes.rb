90fd56abd357b69941c36d90b89254b5959379f7
aspekt112 <aspekt112@gmail.com>
2016-02-20 17:15:00 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-	resources :articles do
-		resources :comments
-	end
+	resources :articles, :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
 	get 'contact/contacts'
